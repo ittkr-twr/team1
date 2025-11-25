2e5901781b0602b5d392ac94ee14ae8a62e20ba9
01-Asa <21002@hakodate.kosen-ac.jp>
2025-11-25 10:25:17 +0900

    ゴールを追加

---
+++ game1/gole.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class gole here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class gole extends Actor
+{
+    /**
+     * Act - do whatever the gole wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public gole()
+    {
+        getImage().scale(200,100);
+    }
+    public void act() 
+    {
+        
+    } 
+    
+}
