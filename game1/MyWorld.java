baf7876c7daad06e46d19fdbfc2323904813d262
佐々木 威真 <21074@hakodate-ct.ac.jp>
2025-12-02 09:44:42 +0900

    ディエゴ追加

--- game1/MyWorld.java
+++ game1/MyWorld.java
@@ -17,12 +17,9 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-
-<<<<<<< HEAD
         Defense d = new Defense();
         addObject(d, 300, 50);
-=======
->>>>>>> 7bdd0919c0f353674126cbc7669b43b254747c1f
+
         addObject( new gole(), 300, 25 );
 
     }
