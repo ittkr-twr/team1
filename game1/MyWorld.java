e29871a0e5882c9cd90c350e369bfbbc9a6493b8
01-Asa <21002@hakodate.kosen-ac.jp>
2025-12-02 09:11:26 +0900

    ゴールの追加

--- game1/MyWorld.java
+++ game1/MyWorld.java
@@ -17,11 +17,9 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-<<<<<<< HEAD
+
         addObject( new gole(), 300, 25 );
-=======
-        
->>>>>>> c35391b284812a9e43d18a9a96164a2e5345b7b5
+
     }
     
     
