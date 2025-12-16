75c24065e31578b1e3b76deae28aa64596b341c8
佐々木 威真 <21074@hakodate-ct.ac.jp>
2025-12-16 09:50:29 +0900

    クリアの追加

--- game1/Rafael.java
+++ game1/Rafael.java
@@ -1,15 +1,28 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
+public class Rafael extends Actor
+{
+    private int speed = 5;
 
-
-public class Rafael extends Actor {
-    public Rafael() {
+    public Rafael()
+    {
         getImage().scale(100, 150);
     }
 
     public void act() 
     {
-        // Add your action code here.
-        
+        moveLeftRight();
+    }
+    private void moveLeftRight()
+    {
+        if (Greenfoot.isKeyDown("left")) {
+            setLocation(getX() - speed, getY());
+        }
+        if (Greenfoot.isKeyDown("right")) {
+            setLocation(getX() + speed, getY());
+        }
+        if (getX() < 50) setLocation(50, getY());
+        if (getX() > getWorld().getWidth() - 50)
+            setLocation(getWorld().getWidth() - 50, getY());
     }
 }
--- game1/ball.java
+++ game1/ball.java
@@ -1,28 +1,24 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class ball here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class ball extends Actor
 {
-    /**
-     * Act - do whatever the ball wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private int speed = 2;
+
     public ball()
     {
         getImage().scale(30,30);
+        setRotation(270);
     }
+
     public void act() 
     {
-        setRotation(270);
-        move(2);
-        Actor actor = getOneIntersectingObject( barrier.class );
-        if( actor != null ){
-            getWorld().removeObject( actor );
+        move(speed);
+
+        if (isTouching(gole.class)) {
+            getWorld().showText("CLEAR!", 300, 200);
+            Greenfoot.stop();
         }
-    }    
-}
+    }
+    
+    }
+
