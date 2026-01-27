b89d454fe97c5a8c85145ba9e9007bd6499a8ff1
ittkr-twr <itoh.takuru@gmail.com>
2026-01-27 09:32:34 +0900

    キック音のドゥルルルウルルルルル現象を修正

--- game1/ball.java
+++ game1/ball.java
@@ -1,18 +1,16 @@
 import greenfoot.*;
-
 public class ball extends Actor
 {
-
     public ball()
     {
         getImage().scale(30,30);
         setRotation(270);
     }
-
     int px= Math.random() < 0.5 ? 3 : -3;
     int py=-3;
     int waitTimer = 99;
-
+    boolean justBouncedFromHantei = false;
+    
     public void act() 
     {
         if (waitTimer > 0) {
@@ -22,22 +20,23 @@ public class ball extends Actor
             return;
         }
         getWorld().showText("", 300, 200);
-
         if (isTouching(gole.class)) {
             Greenfoot.setWorld(new Clear());
             Greenfoot.playSound("clear.mp3");
             Greenfoot.stop();
         }
-
         int x = getX();
         int y = getY();
-
+        
         Actor actor2 = getOneIntersectingObject( Hantei.class );
-        if( actor2 != null ){
+        if( actor2 != null && !justBouncedFromHantei ){
             py=Math.abs(py) * -1;
             Greenfoot.playSound("kick.mp3");
+            justBouncedFromHantei = true;
+        } else if( actor2 == null ){
+            justBouncedFromHantei = false;
         }
-
+        
         Actor actor3 = getOneIntersectingObject( Defense.class );
         if( actor3 != null ){
             Greenfoot.playSound("attack.mp3");
@@ -50,9 +49,7 @@ public class ball extends Actor
                 py = py * -1; 
             }
             getWorld().removeObject( actor3 );
-
             ((MyWorld) getWorld()).decrementDefenseNum();
-
         }
         Actor actor4 = getOneIntersectingObject( barrier.class );
         if( actor4 != null ){
@@ -65,30 +62,23 @@ public class ball extends Actor
                 py = py * -1; 
             }
         }
-
         setLocation(x+px,y+py);
-
         checkEdge();
     }   
-
     private void checkEdge()
     {
         if(getX() <= 0 || getX() >= getWorld().getWidth() -1)
         {
             px=px*-1;
         }
-
         if(getY() <= 0)
         {
             py=py*-1;
         }
-
         if(getY() >= getWorld().getHeight() -1)
         {
-            Greenfoot.setWorld(new Gameover());
+            getWorld().showText("Gameover", 300, 200);
             getWorld().removeObject( this );
-
         }
     }
-
-}    
+}
